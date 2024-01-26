15ac8da50e157c397f5337626507b715d45bdc25
eccentric <zhosan00@list.ru>
2024-01-26 17:23:26 +0300

    Решена 4 задача

--- java-internship-exam/src/main/java/ru/liga/ppr/exam/task_4_stonks/Task4.java
+++ java-internship-exam/src/main/java/ru/liga/ppr/exam/task_4_stonks/Task4.java
@@ -1,17 +1,22 @@
 package ru.liga.ppr.exam.task_4_stonks;
-
+import java.util.Collections;
 import java.util.List;
 
 public class Task4 {
 
     public boolean checkEquals(List<String> stocks1, List<String> stocks2) {
-        for (String i : stocks1) {
-            for (String j : stocks2) {
-                if (i == j);
-            }
+        Collections.sort(stocks1);
+        Collections.sort(stocks2);
 
+        if (stocks1.size() != stocks2.size()) {
+            return false;
         }
 
+        for (int i = 0; i < stocks1.size(); i++) {
+            if (!stocks1.get(i).equals(stocks2.get(i))){
+                return false;
+            }
+        }
+        return true;
     }
-    return false;
 }
