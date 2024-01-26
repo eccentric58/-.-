cbf4461e335205e45b2facfb934bc07e1452d902
eccentric <zhosan00@list.ru>
2024-01-26 19:36:21 +0300

    Решена 5 и 6 задача

--- java-internship-exam/src/main/java/ru/liga/ppr/exam/task_5_reverse/Task5.java
+++ java-internship-exam/src/main/java/ru/liga/ppr/exam/task_5_reverse/Task5.java
@@ -3,8 +3,18 @@ package ru.liga.ppr.exam.task_5_reverse;
 public class Task5 {
 
     public static Node<String> reverse(
-				Node<String> head) {
-        throw new UnsupportedOperationException("Удалите эту строку и напишите реализацию");
+            Node<String> head) {
+        Node<String> temp = null;
+        Node<String> current = head;
+
+        while (current != null) {
+            temp = current.next;
+            current.next = current.prev;
+            current.prev = temp;
+            head = current;
+            current = temp;
+        }
+        return head;
     }
 
 }
