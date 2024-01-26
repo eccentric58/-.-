5c72251390255fdecfe3533fd408f41aee3ea281
eccentric <zhosan00@list.ru>
2024-01-26 14:22:12 +0300

    1 задача завершена

--- java-internship-exam/src/main/java/ru/liga/ppr/exam/task_1_gooseduck/Task1.java
+++ java-internship-exam/src/main/java/ru/liga/ppr/exam/task_1_gooseduck/Task1.java
@@ -1,11 +1,24 @@
 package ru.liga.ppr.exam.task_1_gooseduck;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task1 {
-
-
     public List<String> gooseDuck(int n) {
-        throw new UnsupportedOperationException("Удалите эту строку и напишите реализацию");
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("GooseDuck");
+            } else if (i % 3 == 0) {
+                result.add("Goose");
+            } else if (i % 5 == 0) {
+                result.add("Duck");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
     }
+
+
 }
